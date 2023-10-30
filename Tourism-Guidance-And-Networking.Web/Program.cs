37ca69722d7c39a773c9f95ed8ecdcd21824a610
unknown <mohedehab530@gmail.com>
2023-10-30 13:09:45 +0200

    UpdateInilization

--- Tourism-Guidance-And-Networking.Web/Program.cs
+++ Tourism-Guidance-And-Networking.Web/Program.cs
@@ -1,4 +1,12 @@
 
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Tourism_Guidance_And_Networking.Core.Interfaces;
+using Tourism_Guidance_And_Networking.Core.Models;
+using Tourism_Guidance_And_Networking.DataAccess;
+using Tourism_Guidance_And_Networking.DataAccess.Data;
+
+
 namespace Tourism_Guidance_And_Networking.Web
 {
     public class Program
@@ -13,6 +21,17 @@ namespace Tourism_Guidance_And_Networking.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            builder.Services.AddIdentity<ApplicationUser,IdentityRole>().AddDefaultTokenProviders()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            
 
             var app = builder.Build();
 
--- Tourism_Guidance_And_Networking.DataAccess/Data/ApplicationDbContext.cs
+++ Tourism_Guidance_And_Networking.DataAccess/Data/ApplicationDbContext.cs
@@ -4,5 +4,9 @@ namespace Tourism_Guidance_And_Networking.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
